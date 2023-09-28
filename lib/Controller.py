9060fb73f2460274b0a15a2673473cb4aa6eca04
Aarush <36015585+AarushX@users.noreply.github.com>
2023-09-27 20:46:00 -0400

    e2

--- lib/Controller.py
+++ lib/Controller.py
@@ -2,6 +2,8 @@ import aiohttp
 import asyncio
 import time
 import json
+import atexit
+from itertools import cycle
 
 # Prompts and Intermediate Lists
 from prompts.gpt_prompts import *
@@ -9,27 +11,40 @@ with open("prompts/docs_prompt.txt", "r") as promptFile: SYS_PROMPT = promptFile
 with open("prompts/safety_prompt.txt", "r") as promptFile: SAFETY_SYS_PROMPT = promptFile.read()
 
 class Controller:
-    def __init__(self, api_key):
-        self.api_key = api_key
+    def __init__(self, keys, worker_amt=1):
+        self.key_cycle = cycle(keys)
+
+        self.api_key = keys[0]
         self.headers = {
             "Content-Type": "application/json",
-            "Authorization": "Bearer {}".format(api_key),
+            "Authorization": "Bearer {}".format(self.api_key),
         }
+        self.queue = asyncio.Queue()
+        self.worker_amt = worker_amt
+        self.time_delay = 0.001
+        self.results = {}
+        self.workers = []
+        self.last_api_key_time = time.time()
 
-    async def fetch(self, session, url, headers, data):
-        start = time.time()
-        async with session.post(url, headers=headers, data=data) as request_response:
-            response = await request_response.text()
-            elapsed_time = time.time() - start
-            return response, elapsed_time
+        atexit.register(self.cleanup)
+        
+    async def start_workers (self):
+        self.workers = [asyncio.create_task(self.worker()) for _ in range(self.worker_amt)]
 
     async def chat(
         self,
         model,
         messages,
-        temperature=0.7
+        temperature=0.7,
+        override_headers=None,
+        override_response_data=None,
     ):
+
+        
         url = "https://api.openai.com/v1/chat/completions"
+
+        if override_headers is None:
+            override_headers = self.headers
 
         data = json.dumps(
             {
@@ -39,7 +54,9 @@ class Controller:
             }
         )
         async with aiohttp.ClientSession() as session:
-            response, elapsed_time = await self.keep_fetching(session, url, self.headers, data)
+            response, elapsed_time = await self.fetch(session, url, override_headers, data)
+        if override_response_data is True:
+            return json.loads(response)
         response_text = json.loads(response)["choices"][0]["message"]["content"]
         return response_text, elapsed_time
 
@@ -132,32 +149,52 @@ class Controller:
 
         return generated_messages
 
-    async def keep_fetching (self, session, url, headers, data):
-        request_completed = False
-        time_delay = 0.1
-        while not request_completed:
-            response, elapsed_time = await self.fetch(session, url, headers, data)
+    async def fetch_internal(self, session, url, headers, data):
+        start = time.time()
+        async with session.post(url, headers=headers, data=data) as request_response:
+            response = await request_response.text()
+            elapsed_time = time.time() - start
+            return response, elapsed_time
+
+    async def fetch(self, session, url, headers, data):
+        task_id = id(asyncio.current_task())
+
+        await self.queue.put((task_id, session, url, headers, data))
+
+        while task_id not in self.results:
+            await asyncio.sleep(0.001)  # Wait for the result
+        result = self.results.pop(task_id)
+        return result
+
+    async def worker(self):
+        while True:
+            task_id, session, url, headers, data = await self.queue.get()
+            response, elapsed_time = await self.fetch_internal(session, url, headers, data)
             if "error" in response:
-                print("Delayed", sep=" ")
-                request_completed = False
-                if (time_delay < 5): time_delay *= 2
-                await asyncio.sleep(time_delay)
-                
+                print("Delayed", response)
+                if self.time_delay < 5:
+                    self.time_delay *= 2
+
+                # TODO, CHECK IF THIS MAY BE BETTER AFTER THE SLEEP.
+                await self.shift_api_key(time_recieved=time.time())
+
+                await asyncio.sleep(self.time_delay)
+                await self.queue.put((task_id, session, url, headers, data))  # Put the task back in the queue
             else:
-                print("Not delayed", sep=" ")
-                request_completed = True
-        return response, elapsed_time
+                print("On Time")
+                self.time_delay = 0.001  # Reset the delay
+                self.results[task_id] = (response, elapsed_time)
+            self.queue.task_done()
 
+    async def shift_api_key (self, time_recieved):
+        if (time_recieved - self.last_api_key_time) > 0.05:
+            self.api_key = next(self.key_cycle)
+            self.last_api_key_time = time_recieved
+        self.headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer {}".format(self.api_key),
+        }
 
-# async def main():
-#     controller = APIController("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")
-#     prompt = "Run to end, turn left, before that fly up 60 feet and you fly up enable the speaker"
-#     commands = await controller.split_prompt(prompt)
-#     print(commands)
-#     # rated_commands = await controller.rate_complexity(commands)
-#     # assigned_commands = controller.assign_models(rated_commands)
-#     # await controller.execute_tasks(assigned_commands)
-
-
-# loop = asyncio.get_event_loop()
-# loop.run_until_complete(main())
+    def cleanup(self):
+        for worker in self.workers:
+            worker.cancel()
--- lib/GPTTools.py
+++ lib/GPTTools.py
@@ -8,24 +8,53 @@ import os
 
 from Controller import Controller as new_Controller
 
+
+#### bad
+# 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 
+# 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
+# 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
+
+#### good
+# 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
 # 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
-# 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 (bad)
-# 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
+
+keys = [
+    "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"
+    "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",
+]
+
+controller = new_Controller(
+    keys,
+    worker_amt=11,
+)
 
 
-## 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
 
-controller = new_Controller("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")
+
+
 
 async def main():
-    api_key = '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'
+    await controller.start_workers()
 
-    prompt = 'Please fly up 6 feet, then turn in a circle with radius 10 feet with the intial point being the center of the circle, but before you do anything else takeoff the drone please'
+    for key in keys:
+        response = await controller.chat(
+            "gpt-4",
+            [{"role": "user", "content": "Hello, how are you?"}],
+            override_headers={
+                "Content-Type": "application/json",
+                "Authorization": "Bearer {}".format(key),
+            },
+            override_response_data=True,
+        )
+        print(key, response)
 
-    models = {
-        'gpt-4': 1,
-        'gpt-3.5-turbo-16k': 10
-    }
+    api_key = "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"
+
+    
+
+    prompt = "Please fly up 6 feet, then turn in a circle with radius 10 feet with the intial point being the center of the circle, but before you do anything else takeoff the drone please"
+
+    models = {"gpt-4": 5, "gpt-3.5-turbo-16k": 5}
 
     queue = []
     index = 0
@@ -34,24 +63,37 @@ async def main():
 
     for model, count in models.items():
         for _ in range(count):
-            queue.append(asyncio.ensure_future(controller.designate(split_prompt, model, split_prompt=True, iteration=index)))
+            queue.append(
+                asyncio.ensure_future(
+                    controller.designate(
+                        split_prompt, model, split_prompt=True, iteration=index
+                    )
+                )
+            )
             index += 1
 
     responses_bunched = await asyncio.gather(*queue)
 
     responses = []
     for resp in responses_bunched:
-        responses += resp;
-    
-    filename = f'./data/model_data_{int(time.time()) }.csv'
+        responses += resp
+
+    filename = f"./data/model_data_{int(time.time()) }.csv"
     os.makedirs(os.path.dirname(filename), exist_ok=True)
-    with open(filename, 'w', newline='') as csvfile:
-        fieldnames = ['Iteration', 'Model', 'Request Time', 'Safety Check Time', 'Total Time', 'Safety Rating', 'Code']
+    with open(filename, "w", newline="") as csvfile:
+        fieldnames = [
+            "Iteration",
+            "Model",
+            "Request Time",
+            "Safety Check Time",
+            "Total Time",
+            "Safety Rating",
+            "Code",
+        ]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
         writer.writeheader()
         for response in responses:
-
             request_time = float(response["request_time"])
 
             code = response["response"]
@@ -59,19 +101,30 @@ async def main():
             iteration = response["iteration"]
 
             total_time = request_time
-            
+
             if "safety_check_time" in response:
                 safety_check_time = float(response["safety_check_time"])
                 safety_rating = response["safety_check_response"]
-                
+
                 total_time += safety_check_time
             else:
                 safety_check_time = "N/A"
                 safety_rating = "N/A"
 
-            writer.writerow({'Iteration': iteration, 'Model': model, 'Request Time': round(request_time, 2), 'Safety Check Time': safety_check_time, 'Total Time': round(total_time, 2), 'Safety Rating': safety_rating, 'Code': code})
+            writer.writerow(
+                {
+                    "Iteration": iteration,
+                    "Model": model,
+                    "Request Time": round(request_time, 2),
+                    "Safety Check Time": safety_check_time,
+                    "Total Time": round(total_time, 2),
+                    "Safety Rating": safety_rating,
+                    "Code": code,
+                }
+            )
 
             print(f"{model} in {round(total_time, 2)} ")
-        
+
+
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
